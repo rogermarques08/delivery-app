f3bc9a1f9130c6e729c8ac8f11b38ad85a45176d
Roger Marques <marquesroger226@gmail.com>
2023-05-03 19:15:29 -0300

    fix: conflict

--- back-end/src/database/models/SalesModel.js
+++ back-end/src/database/models/SalesModel.js
@@ -1,19 +1,22 @@
 module.exports = (sequelize, DataTypes) => {
-  const Sale = sequelize.define('Sale', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true,},
+  const Sale = sequelize.define(
+'Sale', 
+{
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     userId: { type: DataTypes.INTEGER, foreign: true, allowNull: false },
     sellerId: { type: DataTypes.INTEGER, foreign: true, allowNull: false },
     totalPrice: DataTypes.DECIMAL(9, 2),
-    deliveryAddress: {type: DataTypes.STRING,  allowNull: false },
-    deliveryNumber: {type: DataTypes.STRING,  allowNull: false },
-    saleDate: {type: DataTypes.DATE, allowNull: false},
-    status: {type: DataTypes.STRING,  allowNull: false},
+    deliveryAddress: { type: DataTypes.STRING, allowNull: false },
+    deliveryNumber: { type: DataTypes.STRING, allowNull: false },
+    saleDate: { type: DataTypes.DATE, allowNull: false },
+    status: { type: DataTypes.STRING, allowNull: false },
   },
-  {
+{
     timestamps: false,
     underscored: true,
     tableName: 'sales',
-  });
+  },
+);
 
   // Sale.associate = (models) => {
   //   models.User.belongsToMany(models.User,
@@ -25,4 +28,4 @@ module.exports = (sequelize, DataTypes) => {
   //     });
   // }
   return Sale;
-}
+};
