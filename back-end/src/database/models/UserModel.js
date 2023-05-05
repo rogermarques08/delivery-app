ea1169ef639f2bf6d928c03ae1f5537d569f0efc
Roger Marques <marquesroger226@gmail.com>
2023-05-05 18:34:52 -0300

    reafec: add default value for role

--- back-end/src/database/models/UserModel.js
+++ back-end/src/database/models/UserModel.js
@@ -1,37 +1,48 @@
 const Sale = require('./SalesModel');
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  const User = sequelize.define(
+'User', 
+{
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    role: DataTypes.STRING,
+    role: {type: DataTypes.STRING, defaultValue: 'customer'}
   },
-  {
+{
     timestamps: false,
     underscored: true,
     tableName: 'users',
-  })
+  },
+);
 
   User.associate = (models) => {
-    User.hasMany(models.Sale,
-      {foreignKey: 'userId', as: 'user'});
-  }
+    User.hasMany(
+models.Sale,
+      { foreignKey: 'userId', as: 'user' },
+);
+  };
 
   User.associate = (models) => {
-    User.hasMany(models.Sale,
-      {foreignKey: 'sellerId', as: 'seller'});
-  }
+    User.hasMany(
+models.Sale,
+      { foreignKey: 'sellerId', as: 'seller' },
+);
+  };
 
   Sale.associate = (models) => {
-    Sale.belongsTo(models.User,
-      {foreignKey: 'userId', as: 'user'});
-  }
+    Sale.belongsTo(
+models.User,
+      { foreignKey: 'userId', as: 'user' },
+);
+  };
 
   Sale.associate = (models) => {
-    Sale.belongsTo(models.User,
-      {foreignKey: 'sellerId', as: 'seller'});
-  }
+    Sale.belongsTo(
+models.User,
+      { foreignKey: 'sellerId', as: 'seller' },
+);
+  };
   return User;
-}
+};
