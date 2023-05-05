fd38422e8e54fd42971e5e57dfb8da0efb63b03c
Roger Marques <marquesroger226@gmail.com>
2023-05-05 18:11:49 -0300

    feat: add data-testid on inputs

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,6 +1,56 @@
+import { useState } from 'react';
+
 function Register() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = ({ target: { value, name } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
   return (
-    <h1>Registro</h1>
+    <div>
+      <h2>Cadastro</h2>
+      <form>
+        <label htmlFor="common_register__input-name">
+          Name
+          <input
+            type="text"
+            name="name"
+            data-testid="common_register__input-name"
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="common_register__input-email">
+          Email
+          <input
+            type="text"
+            name="email"
+            data-testid="common_register__input-email"
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="common_register__input-password">
+          Senha
+          <input
+            type="text"
+            name="password"
+            onChange={ handleChange }
+            data-testid="common_register__input-password"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="common_register__button-register"
+        >
+          CADASTRO
+        </button>
+        <p data-testid="common_register__element-invalid_register ">error message</p>
+      </form>
+    </div>
   );
 }
 
