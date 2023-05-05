3417b4fcb41408b2d560930d89e32876913e687d
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-05-05 14:28:21 -0300

    feat: adding token

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -29,17 +29,16 @@ const createNewUser = async (req, res) => {
   if (hasEmail) {
     return res.status(409).json({ message: 'Conflict' });
   }
-  // const newUser = 
-  await UserService.createNewUser({ 
+  const newUser = await UserService.createNewUser({ 
     name, 
     email, 
     password: md5(password),
     role,
    });
 
-  // const payload = createToken({ name, email, password, role, id: newUser.id });
-  // console.log(payload);
-  return res.status(201).json({ message: 'Created' });
+  const payload = createToken({ name, email, password, role, id: newUser.id });
+  
+  return res.status(201).json({ token: payload });
 } catch (e) {
   console.log(e);
   return res.status(500).json({ message: 'internal error', error: e.message });
