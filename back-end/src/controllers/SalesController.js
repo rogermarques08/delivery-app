7435653c10c3839c73232bf43b469fd330c6f381
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-05-08 09:10:49 -0300

    feat: created sale route post

--- back-end/src/controllers/SalesController.js
+++ back-end/src/controllers/SalesController.js
@@ -1,19 +1,25 @@
 const SalesService = require('../services/SalesService');
 
+const findById = async (req, res) => {
+  const { userId } = req.body;
+  const findUser = await SalesService.findById(userId);
+  return res.status(200).json({ findUser });
+};
+
 const createSale = async (req, res) => {
   const { userId, 
-    sellerId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status } = req.body;
+    sellerId, totalPrice, deliveryAddress, deliveryNumber, quantity, productId } = req.body;
     try {
-      const newSale = await SalesService.createSale(
+      const newSale = await SalesService.createSale({
         userId, 
         sellerId, 
         totalPrice, 
         deliveryAddress, 
         deliveryNumber, 
-        saleDate, 
-        status,
-        );
-        return res.status(201).json({ newSale });
+        quantity, 
+        productId,
+      });
+        return res.status(201).json(newSale);
     } catch (e) {
       console.log(e);
       return res.status(500).json({ message: 'internal error', error: e.message });
@@ -21,5 +27,6 @@ const createSale = async (req, res) => {
 };
 
 module.exports = {
+  findById,
   createSale,
 };
--- back-end/src/services/SalesService.js
+++ back-end/src/services/SalesService.js
@@ -1,13 +1,30 @@
 const { Sale } = require('../database/models');
+const { User } = require('../database/models');
 
-const createSale = async ({ 
-  userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status }) => {
+// const findByIdUser = async (userId) => {
+//   const findUser = await User.findByPk({ where: userId });
+//   return findUser;
+// };
+
+// const findByIdSeller = async (sellerId) => {
+//   const findSeller = await User.findByPk({ where: sellerId });
+//   return findSeller;
+// };
+
+const createSale = async (saleBody) => {
+await User.findByPk(saleBody.userId);
+await User.findByPk(saleBody.sellerId);
+
   const newSale = await Sale.create({
-    userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status,
+    ...saleBody,
+    saleDate: new Date(), 
+    status: 'pendente',
   });
   return newSale;
 };
 
 module.exports = {
+  // findByIdUser,
+  // findByIdSeller,
   createSale,
 };
