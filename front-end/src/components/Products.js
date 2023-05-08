56b1c81aaad9b2eca41c1fe359b940944713412e
Roger Marques <marquesroger226@gmail.com>
2023-05-08 16:54:13 -0300

    reafac: returning user token

---
+++ front-end/src/components/Products.js
@@ -0,0 +1,13 @@
+function Products() {
+//   const [products, setProducts] = useState([]);
+
+  //   useEffect( async() => {
+  //     const products = await getData('GET', '', '/customer/products')
+  //   }, []);
+
+  return (
+    <div />
+  );
+}
+
+export default Products;
