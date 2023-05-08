03d8b3c275271e9a601b67e5a8059e3eefaa5b40
mAngelo-dev <m.angelodev@gmail.com>
2023-05-08 19:55:40 -0300

    test: :test_tube: Add tests
    Creates tests for back-end services in /login:POST

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -14,38 +14,37 @@ const login = async (req, res) => {
     if (user.password !== md5(password)) {
       return res.status(404).json({ message: 'Not found' });
     }
-      return res.status(200).json(user);
+    return res.status(200).json(user);
   } catch (e) {
-    console.log(e);
     return res.status(500).json({ message: 'internal error', error: e.message });
   }
 };
 
 const createNewUser = async (req, res) => {
   try {
-  const { name, email, password, role } = req.body;
+    const { name, email, password, role } = req.body;
 
-  const hasEmail = await UserService.login(email);
-  if (hasEmail) {
-    return res.status(409).json({ message: 'Conflict' });
+    const hasEmail = await UserService.login(email);
+    if (hasEmail) {
+      return res.status(409).json({ message: 'Conflict' });
+    }
+    const newUser = await UserService.createNewUser({
+      name,
+      email,
+      password: md5(password),
+      role,
+    });
+
+    const payload = createToken({ name, email, password, role, id: newUser.id });
+
+    return res.status(201).json({ token: payload });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: 'internal error', error: e.message });
   }
-  const newUser = await UserService.createNewUser({ 
-    name, 
-    email, 
-    password: md5(password),
-    role,
-   });
-
-  const payload = createToken({ name, email, password, role, id: newUser.id });
-  
-  return res.status(201).json({ token: payload });
-} catch (e) {
-  console.log(e);
-  return res.status(500).json({ message: 'internal error', error: e.message });
-}
 };
- 
-module.exports = { 
+
+module.exports = {
   login,
   createNewUser,
- };
+};
---
+++ back-end/src/tests/login.route.test.js
@@ -0,0 +1,40 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const sinon = require('sinon')
+const app = require('../api/app')
+const { User } = require('../database/models');
+const {
+  validLogin,
+  zebirita,
+  invalidLogin,
+  invalidLogin2 } = require('./mocks/login.mocks');
+
+
+const { expect } = chai
+
+chai.use(chaiHttp);
+
+describe('Test route /login - Back End', () => {
+  beforeEach(() => sinon.restore());
+
+  it('/login:POST with incorrect login', async () => {
+    sinon.stub(User, 'findOne').resolves()
+    const chaihttpResponse = await chai.request(app).post('/login').send(invalidLogin2);
+    expect(chaihttpResponse.status).to.be.equal(404);
+    expect(chaihttpResponse.body).to.be.deep.equal({ message: 'Not found' });
+  });
+
+  it('/login:POST with valid email but invalid password', async () => {
+    sinon.stub(User, 'findOne').resolves(zebirita)
+    const chaihttpResponse = await chai.request(app).post('/login').send(invalidLogin);
+    expect(chaihttpResponse.status).to.be.equal(404);
+    expect(chaihttpResponse.body).to.be.deep.equal({ message: 'Not found' });
+  });
+
+  it('/login:POST with correct login', async () => {
+    sinon.stub(User, 'findOne').resolves(zebirita)
+    const chaihttpResponse = await chai.request(app).post('/login').send(validLogin);
+    expect(chaihttpResponse.status).to.be.equal(200);
+    expect(chaihttpResponse.body).to.be.deep.equal(zebirita);
+  });
+});
