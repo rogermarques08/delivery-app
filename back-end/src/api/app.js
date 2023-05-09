40cd1c15c629e5d5e99ed711d2d426b8690beff6
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-05-09 09:36:41 -0300

    fix: resolve conflict merge, done - up

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const UserRoutes = require('../routes/UserRoutes');
 const routerRegister = require('../routes/RegisterRoutes');
 const customerRoutes = require('../routes/customerRoutes');
+const routerNewSales = require('../routes/SalesCheckoutRoutes');
 
 const app = express();
 app.use(express.json());
@@ -13,4 +14,5 @@ app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', UserRoutes);
 app.use('/register', routerRegister);
 app.use('/customer', customerRoutes);
+app.use('/checkout', routerNewSales);
 module.exports = app;
