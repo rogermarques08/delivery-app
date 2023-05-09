2b306f771dc7522bb1101c42c8323ab4fa85e461
Roger Marques <marquesroger226@gmail.com>
2023-05-08 22:05:10 -0300

    feat: adding products cards on screen

--- front-end/src/components/Products.js
+++ front-end/src/components/Products.js
@@ -1,12 +1,47 @@
-function Products() {
-//   const [products, setProducts] = useState([]);
+import { useContext } from 'react';
+import DeliveryContext from '../context/DeliveryContext';
 
-  //   useEffect( async() => {
-  //     const products = await getData('GET', '', '/customer/products')
-  //   }, []);
+function Products() {
+  const { products } = useContext(DeliveryContext);
 
   return (
-    <div />
+    <div>
+      {products?.map((product) => (
+        <div key={ product.id }>
+          <h1
+            data-testid={ `customer_products__element-card-title-${product.id}` }
+          >
+            {product.name}
+          </h1>
+          <img
+            src={ product.urlImage }
+            alt={ product.name }
+            data-testid={ `customer_products__img-card-bg-image-${product.id}` }
+          />
+          <p
+            data-testid={ `customer_products__element-card-price-${product.id}` }
+          >
+            {product.price}
+          </p>
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+          >
+            -
+          </button>
+          <input
+            type="number"
+            data-testid={ `customer_products__input-card-quantity-${product.id}` }
+          />
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-add-item-${product.id}` }
+          >
+            +
+          </button>
+        </div>
+      ))}
+    </div>
   );
 }
 
--- front-end/src/pages/CustomerProducts.js
+++ front-end/src/pages/CustomerProducts.js
@@ -1,9 +1,25 @@
+import { useContext, useEffect } from 'react';
 import NavBar from '../components/NavBar';
+import Products from '../components/Products';
+import DeliveryContext from '../context/DeliveryContext';
+import { getProducts } from '../utils/getData';
+import getLocalStorage from '../utils/getLocalStorage';
 
 function CustomerProducts() {
+  const { setProducts } = useContext(DeliveryContext);
+
+  useEffect(() => {
+    const { token } = getLocalStorage('user');
+
+    getProducts('GET', '/customer/products', token).then((data) => {
+      setProducts(data);
+    });
+  }, [setProducts]);
+
   return (
     <header>
       <NavBar />
+      <Products />
     </header>
   );
 }
