e7d65e7b6bd36c7575d859c59d061decf9be1231
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-05-14 18:00:27 -0300

    feat: adding route get all to create users table in front

--- back-end/src/routes/AdminRoutes.js
+++ back-end/src/routes/AdminRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
-const adminAuth = require('../middlewares/adminAuth');
+// const adminAuth = require('../middlewares/adminAuth');
 const AdminController = require('../controllers/AdminController');
+const adminMiddleware = require('../middlewares/adminAuth');
 
 const AdminRouter = express.Router();
 
-AdminRouter.post('/', adminAuth, UserController.createNewUser);
-AdminRouter.delete('/:id', adminAuth, AdminController.adminRemove);
+AdminRouter.post('/', adminMiddleware, UserController.createNewUser);
+AdminRouter.delete('/:id', adminMiddleware, AdminController.adminRemove);
+AdminRouter.get('/', adminMiddleware, AdminController.getAllUsers);
 
 module.exports = AdminRouter;
